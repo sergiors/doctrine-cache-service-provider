86c44244004b115b540fbfa217741bd8113144d9
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2015-10-22 17:21:56 -0200

    initial commit

---
+++ src/Provider/DoctrineCacheServiceProvider.php
@@ -0,0 +1,78 @@
+<?php
+namespace Inbep\Silex\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\Common\Cache\RedisCache;
+use Doctrine\Common\Cache\ApcCache;
+use Doctrine\Common\Cache\XcacheCache;
+
+/**
+ * @author Sérgio Rafael Siqueira <sergio@inbep.com.br>
+ */
+class DoctrineCacheServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        $app['cache.options'] = [
+            'driver' => 'array'
+        ];
+
+        $app['cache.array'] = $app->protect(function () {
+            return new ArrayCache();
+        });
+
+        $app['cache.apc'] = $app->protect(function () {
+            return new ApcCache();
+        });
+
+        $app['cache.redis'] = $app->protect(function ($options) {
+            if (empty($options['host']) || empty($options['port'])) {
+                throw new \RuntimeException('You must specify "host" and "port" for Redis.');
+            }
+
+            $redis = new \Redis();
+            $redis->connect($options['host'], $options['port']);
+
+            if (isset($options['password'])) {
+                $redis->auth($options['password']);
+            }
+
+            $cache = new RedisCache();
+            $cache->setRedis($redis);
+            return $cache;
+        });
+
+        $app['cache.xcache'] = $app->protect(function () {
+            return new XcacheCache();
+        });
+
+        $app['cache.factory'] = $app->protect(function ($options) use ($app) {
+            switch ($options['driver']) {
+                case 'array':
+                    return $app['cache.array']();
+                    break;
+                case 'apc':
+                    return $app['cache.apc']();
+                    break;
+                case 'redis':
+                    return $app['cache.redis']($options);
+                    break;
+                case 'xcache':
+                    return $app['cache.xcache']();
+                    break;
+            }
+
+            throw new \RuntimeException();
+        });
+
+        $app['cache'] = $app->share(function (Application $app) {
+            return $app['cache.factory']($app['cache.options']);
+        });
+    }
+
+    public function boot(Application $app)
+    {
+    }
+}
---
+++ tests/Provider/DoctrineCacheServiceProviderTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace Inbep\Silex\Provider;
+
+use Silex\Application;
+use Silex\WebTestCase;
+use Silex\Provider\DoctrineServiceProvider;
+use Doctrine\Common\Cache\ApcCache;
+
+class DoctrineCacheServiceProviderTest extends WebTestCase
+{
+    /**
+     * @test
+     */
+    public function register()
+    {
+        $app = $this->createApplication();
+        $app->register(new DoctrineCacheServiceProvider(), [
+            'cache.options' => [
+                'driver' => 'apc'
+            ]
+        ]);
+
+        $this->assertInstanceOf(ApcCache::class, $app['cache']);
+
+    }
+
+    public function createApplication()
+    {
+        $app = new Application();
+        $app['debug'] = true;
+        $app['exception_handler']->disable();
+        return $app;
+    }
+}
